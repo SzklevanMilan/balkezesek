c6c89d8b927d835a4335be2286fd999f6d615e4a
SzklevanMilan <szklevanm@gmail.com>
2020-10-16 11:52:59 +0200

    Add project files.

---
+++ balkezesek/Balkezesek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace balkezesek
+{
+    internal class Balkezesek
+    {
+        public string Nev { get; private set; }
+        public string Elso { get; private set; }
+        public string Utolso { get; private set; }
+        public int Suly { get; private set; }
+        public int Magassag { get; private set; }
+        public Balkezesek(string sor)
+        {
+            string[] adat = sor.Split(';');
+            Nev = adat[0];
+            Elso = adat[1];
+            Utolso = adat[2];
+            Suly = Convert.ToInt32(adat[3]);
+            Magassag = Convert.ToInt32(adat[4]);
+        }
+    }
+}
---
+++ balkezesek/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace balkezesek
+{
+    class Program
+    {
+        static List<Balkezesek> lista = new List<Balkezesek>();
+        static int be = 0;
+        static void Beolvas()
+        {
+            StreamReader sr = new StreamReader("balkezesek.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                lista.Add(new Balkezesek(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+
+        static void Harmadik()
+        {
+            Console.WriteLine("3.feladat: {0}",lista.Count());
+        }
+
+        static void Negyedik()
+        {
+            foreach (var l in lista)
+            {
+                if (l.Utolso.Contains("1999-10"))
+                {
+                    Console.WriteLine($"\t{l.Nev}, {l.Magassag*2.54}cm");
+                }
+            }
+        }
+        static void Otodik()
+        {
+            while (true)
+            {
+                Console.WriteLine("Írjon be egy évszámot 1990 és 1999 között!");
+                be = Convert.ToInt32(Console.ReadLine());
+                if (be >= 1990 && be <= 1999)
+                {
+                    Console.WriteLine("Jó számot adott meg.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hibás adat,kérek egy 1990 és 1999 közötti évszámot.");
+                }
+            }
+        }
+        static void Hatodik()
+        {
+            foreach (var i in lista)
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //var b = new Balkezesek("Jim Abbott","1989-04-08","1999-07-21",200,75);
+            Beolvas();
+            Harmadik();
+            Console.WriteLine("4.feladat:");
+            Negyedik();
+            Console.WriteLine("5.feladat:");
+            Otodik();
+            Console.WriteLine("6.feladat:");
+            Hatodik();
+            Console.ReadLine();
+        }
+    }
+}
